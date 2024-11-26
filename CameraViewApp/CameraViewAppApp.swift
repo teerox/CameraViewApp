2f3ca1b60d05c3a2ee8c8691e034d43db380695d
teerox44 <anthony@publiclyme.com>
2024-11-26 17:09:26 -0400

    Initial Commit

---
+++ CameraViewApp/CameraViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraViewAppApp.swift
+//  CameraViewApp
+//
+//  Created by Anthony Odu on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CameraViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
